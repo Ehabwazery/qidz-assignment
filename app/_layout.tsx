af058c4c4cceeb8542039c266f718fcbbaca54d9
Ehabwazery <ehab.elwazery19@gmail.com>
2025-04-03 21:55:19 +0200

    Add mobx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
+import { StoreProvider } from "./context/StoreContext";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <StoreProvider>
+      <Stack />
+    </StoreProvider>
+  );
 }
---
+++ app/stores/index.ts
@@ -0,0 +1,6 @@
+import RootStore from './RootStore';
+
+const rootStore = new RootStore();
+
+export { rootStore };
+export type RootStoreType = typeof rootStore;
